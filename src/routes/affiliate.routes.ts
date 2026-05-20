966ba401e39e6cee46fe7f26595d7c9cd6d5d6e6
Alonso Garcia Espinoza <Elintokable123>
2026-05-19 22:58:29 -0300

    .

--- src/routes/affiliate.routes.ts
+++ src/routes/affiliate.routes.ts
@@ -1,15 +1,13 @@
 import { Router } from 'express'
 import * as AffiliateController from '../controllers/affiliate.controller'
 import * as UserController from '../controllers/user.controller'
-import { requireAdmin } from '../middleware/requireAdmin'
 
 const router = Router()
 
 router.get('/', AffiliateController.index)
 router.get('/create', AffiliateController.createAffiliateForm)
-router.post('/user/:id/deactivate', requireAdmin, UserController.deactivateUserAction)
-router.post('/user/:id/activate', requireAdmin, UserController.activateUserAction)
-
+router.post('/user/:id/deactivate', UserController.deactivateUserAction)
+router.post('/user/:id/activate', UserController.activateUserAction)
 router.get('/:id', AffiliateController.showAffiliateById)
 router.post('/', AffiliateController.createAffiliateAction)
 router.get('/:id/edit', AffiliateController.editAffiliateForm)
